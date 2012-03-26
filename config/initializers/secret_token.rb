491c6d4107aa1245bc0a8932220f2323d49b379c
Daniel Andrade <dam@unixmexico.org>
2012-03-25 18:47:25 -0600

    Initialized Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '50c0eff3badd895f23227ee9d8337b2d7db8088c1317f271ffad5c6b4075cfe93a334b1bb497d7340f3374168e269550e1fb8e61fb7691aa66b5ba705f98e397'
